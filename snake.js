4c3aa49cba100a95528ab958f4b3bad67f742431
khyati0812 <128845739+khyati0812@users.noreply.github.com>
2023-08-01 00:30:18 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -30,7 +30,7 @@ let yVelocity = 0;
 
 let score = 0;
 
-const gulpSound = new Audio("gulp.mp3");
+
 
 //game loop
 function drawGame() {
@@ -154,7 +154,7 @@ function checkAppleCollision() {
     appleY = Math.floor(Math.random() * tileCount);
     tailLength++;
     score++;
-    gulpSound.play();
+    
   }
 }
 
